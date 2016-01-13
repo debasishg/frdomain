d828ea0840b5b6075b7ab04343fd62f7d442cb04
debasishg <ghosh.debasish@gmail.com>
2016-01-13 21:24:54 +0530

    Started chapter 9

---
+++ src/main/scala/frdomain/ch9/Account.scala
@@ -0,0 +1,48 @@
+package frdomain.ch9
+package domain
+package model
+
+import org.joda.time.DateTime
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scalaz._
+import Scalaz._
+import Kleisli._
+
+case class Account(no: String, name: String, idNo: String, dateOpened: DateTime, dateClosed: Option[DateTime])
+
+
+trait AccountService {
+  type Error = String
+
+  def open(no: String, name: String, idNo: String, dateOpened: DateTime): Error \/ Account = {
+    val isValid: Boolean = verifyId(idNo, name)
+    if (isValid) Account(no, name, idNo, dateOpened, None).right
+    else s"Id ($idNo) validation failed".left
+  }
+
+  def verifyId(idNo: String, name: String): Boolean = {
+    // possibly an expensive call that needs to communicate
+    // with external systems, maybe over a Web service
+    // stubbed here
+    true
+  }
+
+  trait IdVerifier {
+    def verifyId(idNo: String, name: String): Boolean
+  }
+
+  case class IdVerification() extends IdVerifier {
+    def verifyId(idNo: String, name: String): Boolean = {
+      //..
+      true
+    }
+  }
+
+  type ErrorOr[A] = Error \/ A
+  def open1(no: String, name: String, idNo: String, dateOpened: DateTime) = kleisli[ErrorOr, IdVerifier, Account] { (v: IdVerifier) =>
+    if (v.verifyId(idNo, name)) Account(no, name, idNo, dateOpened, None).right
+    else s"Id ($idNo) validation failed".left
+  }
+}
