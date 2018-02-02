ba74ae37a58d3c68ffab20dd305fe17eb1cab681
jeff <jefferson.zapata.sierra@gmail.com>
2018-02-02 15:54:13 -0500

    decoupled the AccountService contract from the AccountRepository implementation

--- src/main/scala/frdomain/ch3/repository/reader/AccountService.scala
+++ src/main/scala/frdomain/ch3/repository/reader/AccountService.scala
@@ -6,15 +6,15 @@ import java.util.Date
 import util.{ Try, Success, Failure }
 import common._
 
-trait AccountService[Account, Amount, Balance] {
-  def open(no: String, name: String, openingDate: Option[Date]): Reader[AccountRepository, Try[Account]]
-  def close(no: String, closeDate: Option[Date]): Reader[AccountRepository, Try[Account]]
-  def debit(no: String, amount: Amount): Reader[AccountRepository, Try[Account]]
-  def credit(no: String, amount: Amount): Reader[AccountRepository, Try[Account]]
-  def balance(no: String): Reader[AccountRepository, Try[Balance]]
+trait AccountService[Account, Amount, Balance, Repository] {
+  def open(no: String, name: String, openingDate: Option[Date]): Reader[Repository, Try[Account]]
+  def close(no: String, closeDate: Option[Date]): Reader[Repository, Try[Account]]
+  def debit(no: String, amount: Amount): Reader[Repository, Try[Account]]
+  def credit(no: String, amount: Amount): Reader[Repository, Try[Account]]
+  def balance(no: String): Reader[Repository, Try[Balance]]
 }
 
-object AccountService extends AccountService[Account, Amount, Balance] {
+object AccountService extends AccountService[Account, Amount, Balance, AccountRepository] {
 
   def open(no: String, name: String, openingDate: Option[Date]) = Reader { (repo: AccountRepository) =>
     repo.query(no) match {
